cd29196cb2bc121a7fa0b043c1098464ceb06f26
Iulian David <david.iulian@gmail.com>
2017-08-29 07:12:25 +0300

    added rounded button

---
+++ SocketIO Chat/View/RoundedButton.swift
@@ -0,0 +1,24 @@
+//
+//  RoundedButton.swift
+//  SocketIO Chat
+//
+//  Created by iulian david on 8/29/17.
+//  Copyright © 2017 iulian david. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+          setupView()
+        }
+    }
+
+    private func setupView() {
+        layer.cornerRadius = cornerRadius
+        clipsToBounds = true
+    }
+}
